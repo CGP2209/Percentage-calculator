803dd296ada9ca6ea613a16210c7fd2d58afe22f
Casper <100117982+CGP2209@users.noreply.github.com>
2022-02-21 11:59:29 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Procentregning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Indtast beløb");
+            Double beløb = Convert.ToDouble(Console.ReadLine());
+            
+            Console.WriteLine("Indtast moms");
+            double moms = Convert.ToDouble(Console.ReadLine()) / 100;
+          
+            double samletbeløb = beløb + (beløb * moms);
+            Console.WriteLine("Samlet beløb inkl moms er : {0}", samletbeløb);
+            
+            Console.ReadLine();
+
+        }
+    }
+}
